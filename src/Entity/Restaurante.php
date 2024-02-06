684ac7abf21c6ae9b9349def1acfb7cab9461619
Pablo Vacas <pablovacasgarcia@gmail.com>
2024-02-06 21:49:08 +0100

    Creada entidad restaurante

---
+++ src/Entity/Restaurante.php
@@ -0,0 +1,110 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\RestauranteRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: RestauranteRepository::class)]
+class Restaurante
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column]
+    private ?int $cp = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $correo = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $clave = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $pais = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $ciudad = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $direccion = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getCp(): ?int
+    {
+        return $this->cp;
+    }
+
+    public function setCp(int $cp): static
+    {
+        $this->cp = $cp;
+
+        return $this;
+    }
+
+    public function getCorreo(): ?string
+    {
+        return $this->correo;
+    }
+
+    public function setCorreo(string $correo): static
+    {
+        $this->correo = $correo;
+
+        return $this;
+    }
+
+    public function getClave(): ?string
+    {
+        return $this->clave;
+    }
+
+    public function setClave(string $clave): static
+    {
+        $this->clave = $clave;
+
+        return $this;
+    }
+
+    public function getPais(): ?string
+    {
+        return $this->pais;
+    }
+
+    public function setPais(string $pais): static
+    {
+        $this->pais = $pais;
+
+        return $this;
+    }
+
+    public function getCiudad(): ?string
+    {
+        return $this->ciudad;
+    }
+
+    public function setCiudad(string $ciudad): static
+    {
+        $this->ciudad = $ciudad;
+
+        return $this;
+    }
+
+    public function getDireccion(): ?string
+    {
+        return $this->direccion;
+    }
+
+    public function setDireccion(string $direccion): static
+    {
+        $this->direccion = $direccion;
+
+        return $this;
+    }
+}
