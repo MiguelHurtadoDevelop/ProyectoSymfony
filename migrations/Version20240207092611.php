9ada12bb669ab2e360b255cc2c507ca25860b555
Jorge Herrero <herrerojorge730@gmail.com>
2024-02-07 10:27:32 +0100

    Create Version20240207092611.php

---
+++ migrations/Version20240207092611.php
@@ -0,0 +1,49 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20240207092611 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE categorias (id INT AUTO_INCREMENT NOT NULL, nombre VARCHAR(50) NOT NULL, descripcion VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE pedidos (id INT AUTO_INCREMENT NOT NULL, restaurante_id INT NOT NULL, fecha DATE NOT NULL, enviado INT NOT NULL, precio DOUBLE PRECISION NOT NULL, INDEX IDX_6716CCAA38B81E49 (restaurante_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE pedidosproductos (id INT AUTO_INCREMENT NOT NULL, pedido_id INT NOT NULL, producto_id INT NOT NULL, unidades INT NOT NULL, INDEX IDX_3B085CFA4854653A (pedido_id), INDEX IDX_3B085CFA7645698E (producto_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE productos (id INT AUTO_INCREMENT NOT NULL, categoria_id INT NOT NULL, nombre VARCHAR(50) NOT NULL, descripcion VARCHAR(255) NOT NULL, peso DOUBLE PRECISION NOT NULL, stock INT NOT NULL, precio INT NOT NULL, INDEX IDX_767490E63397707A (categoria_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE restaurante (id INT AUTO_INCREMENT NOT NULL, correo VARCHAR(60) NOT NULL, clave VARCHAR(60) NOT NULL, pais VARCHAR(50) NOT NULL, cp INT NOT NULL, ciudad VARCHAR(50) NOT NULL, direccion VARCHAR(50) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE messenger_messages (id BIGINT AUTO_INCREMENT NOT NULL, body LONGTEXT NOT NULL, headers LONGTEXT NOT NULL, queue_name VARCHAR(190) NOT NULL, created_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', available_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', delivered_at DATETIME DEFAULT NULL COMMENT \'(DC2Type:datetime_immutable)\', INDEX IDX_75EA56E0FB7336F0 (queue_name), INDEX IDX_75EA56E0E3BD61CE (available_at), INDEX IDX_75EA56E016BA31DB (delivered_at), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE pedidos ADD CONSTRAINT FK_6716CCAA38B81E49 FOREIGN KEY (restaurante_id) REFERENCES restaurante (id)');
+        $this->addSql('ALTER TABLE pedidosproductos ADD CONSTRAINT FK_3B085CFA4854653A FOREIGN KEY (pedido_id) REFERENCES pedidos (id)');
+        $this->addSql('ALTER TABLE pedidosproductos ADD CONSTRAINT FK_3B085CFA7645698E FOREIGN KEY (producto_id) REFERENCES productos (id)');
+        $this->addSql('ALTER TABLE productos ADD CONSTRAINT FK_767490E63397707A FOREIGN KEY (categoria_id) REFERENCES categorias (id)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE pedidos DROP FOREIGN KEY FK_6716CCAA38B81E49');
+        $this->addSql('ALTER TABLE pedidosproductos DROP FOREIGN KEY FK_3B085CFA4854653A');
+        $this->addSql('ALTER TABLE pedidosproductos DROP FOREIGN KEY FK_3B085CFA7645698E');
+        $this->addSql('ALTER TABLE productos DROP FOREIGN KEY FK_767490E63397707A');
+        $this->addSql('DROP TABLE categorias');
+        $this->addSql('DROP TABLE pedidos');
+        $this->addSql('DROP TABLE pedidosproductos');
+        $this->addSql('DROP TABLE productos');
+        $this->addSql('DROP TABLE restaurante');
+        $this->addSql('DROP TABLE messenger_messages');
+    }
+}
