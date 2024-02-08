556422e75a7510d40462e88fe4197538c2db1b39
MiguelHurtado <mhurtadojunior@gmail.com>
2024-02-08 09:03:19 +0100

    CRUD de pedidos

---
+++ src/Form/PedidosType.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Pedidos;
+use App\Entity\Restaurante;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class PedidosType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('fecha')
+            ->add('enviado')
+            ->add('precio')
+            ->add('restaurante', EntityType::class, [
+                'class' => Restaurante::class,
+'choice_label' => 'id',
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Pedidos::class,
+        ]);
+    }
+}
