1d0d7894a88c0445a99863812de810d18cf08fc8
MiguelHurtado <mhurtadojunior@gmail.com>
2024-02-08 17:49:26 +0100

    Actualizaciones en carrito de la compre
    Borrar del carrito, añadir cantidad determinada

--- src/Controller/CarritoController.php
+++ src/Controller/CarritoController.php
@@ -13,16 +13,20 @@ use Symfony\Component\HttpFoundation\Session\SessionInterface;
 class CarritoController extends AbstractController
 {
     #[Route('/carrito', name: 'app_carrito')]
-    public function index(): Response
+    public function index(SessionInterface $session): Response
     {
-        return $this->render('carrito/index.html.twig', [
-            'controller_name' => 'CarritoController',
+        
+        $cart = $session->get('cart', []);
+
+        return $this->render('carrito/carrito.html.twig', [
+            'carrito' =>  $cart,
         ]);
     }
 
     #[Route('/carrito/add/{id}', name: 'app_carrito_add')]
     public function add($id, EntityManagerInterface $entityManager, SessionInterface $session): Response
     {
+
         $productosRepository = $entityManager->getRepository(Productos::class);
         $producto = $productosRepository->findOneById($id);
 
@@ -32,12 +36,38 @@ class CarritoController extends AbstractController
             );
         }
 
+        if($producto->getStock() == 0){
+            throw $this->createNotFoundException(
+                'No hay stock del producto con el id: ' . $id
+            );
+        }
+
+        $productoCarrito = [
+            'id' => $producto->getId(),
+            'nombre' => $producto->getNombre(),
+            'precio' => $producto->getPrecio(),
+            'imagen' => $producto->getImagen(),
+            'cantidad' => 1
+        ];
         $cart = $session->get('cart', []);
-        $cart[] = $producto;
+        $cart[] = $productoCarrito;
         $session->set('cart', $cart);
 
         return $this->render('carrito/carrito.html.twig', [
             'carrito' =>  $cart,
         ]);
+    }
+
+    #[Route('/carrito/borrar/{id}', name: 'app_carrito_borrar')]
+    public function borrar($id,EntityManagerInterface $entityManager, SessionInterface $session): Response
+    {
+        $cart = $session->get('cart', []);
+        $cart = array_filter($cart, function ($producto) use ($id) {
+            return $producto['id'] != $id;
+        });
+
+        $session->set('cart', $cart);
+
+        return $this->redirectToRoute('app_carrito');
     }
 }
