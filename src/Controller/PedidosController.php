65aadab9abaf88a64cd4355aba519217a8643e37
Jorge Herrero <herrerojorge730@gmail.com>
2024-02-08 19:27:47 +0100

    vista carrito

--- src/Controller/PedidosController.php
+++ src/Controller/PedidosController.php
@@ -75,7 +75,6 @@ class PedidosController extends AbstractController
             $entityManager->remove($pedido);
             $entityManager->flush();
         }
-
         return $this->redirectToRoute('app_pedidos_index', [], Response::HTTP_SEE_OTHER);
     }
 }
